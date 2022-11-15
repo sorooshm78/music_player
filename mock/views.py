0ef8f508a24c2fc570bb4d83d0daacfca5b76041
Soroush mohammadi <sorooshm78@gmail.com>
2022-11-15 17:19:31 +0330

    reprisent index and detail html file

--- mock/views.py
+++ mock/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class Home(TemplateView):
+    template_name = 'music/index.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["all_albums"] = [
+            {
+                'id':'5',
+                'album_logo' : '',
+                'album_title' : 'shecast eshgi',
+                'artist' : 'Dj soroosh'
+            },
+            {
+                'id':'6',
+                'album_logo' : '',
+                'album_title' : 'ghafas',
+                'artist' : 'Dj mehran'
+            },
+        ]
+        return context
+    
+class Detail(TemplateView):
+    template_name = 'music/detail.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["album"] = {
+            'id':'5',
+            'album_logo' : '',
+            'album_title' : 'shecast eshgi',
+            'artist' : 'Dj soroosh',
+            'song_set' : {
+                'all': [
+                    {
+                        'song_title':'jjjj',
+                        'file_type':'mp3',
+                        'is_favorite':True,
+                    },
+                ]
+            }
+        } 
+        return context
+    
