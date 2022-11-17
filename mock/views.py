3511fb71b92b32e24bae990813de573a29fe32fc
Soroush mohammadi <sorooshm78@gmail.com>
2022-11-17 17:57:08 +0330

    template run with mock server

--- mock/views.py
+++ mock/views.py
@@ -3,43 +3,46 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class Home(TemplateView):
-    template_name = 'music/index.html'
+    template_name = "music/index.html"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["all_albums"] = [
+        context["albums"] = [
             {
-                'id':'5',
-                'album_logo' : '',
-                'album_title' : 'shecast eshgi',
-                'artist' : 'Dj soroosh'
+                "id": "5",
+                "album_logo": "",
+                "album_title": "shecast eshgi",
+                "artist": "Dj soroosh",
             },
             {
-                'id':'6',
-                'album_logo' : '',
-                'album_title' : 'ghafas',
-                'artist' : 'Dj mehran'
+                "id": "6",
+                "album_logo": "",
+                "album_title": "ghafas",
+                "artist": "Dj mehran",
             },
         ]
+        context["user"] = {"username": "sina"}
         return context
-    
+
+
 class Detail(TemplateView):
-    template_name = 'music/detail.html'
+    template_name = "music/detail.html"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["album"] = {
-            'id':'5',
-            'album_logo' : '',
-            'album_title' : 'shecast eshgi',
-            'artist' : 'Dj soroosh',
-            'song_set' : {
-                'all': [
+            "id": "5",
+            "album_logo": "",
+            "album_title": "shecast eshgi",
+            "artist": "Dj soroosh",
+            "song_set": {
+                "all": [
                     {
-                        'song_title':'jjjj',
-                        'file_type':'mp3',
-                        'is_favorite':True,
+                        "song_title": "jjjj",
+                        "file_type": "mp3",
+                        "is_favorite": True,
                     },
                 ]
-            }
-        } 
+            },
+        }
         return context
-    
