5db8439a13e321a18155e0e8bb9e198a887bc6d2
Soroush mohammadi <sorooshm78@gmail.com>
2022-11-18 17:30:53 +0330

    add Authentication and Authorization to view

--- music/models.py
+++ music/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Album(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     album_title = models.CharField(max_length=250)
     album_logo = models.ImageField(
         upload_to="media/albums",
--- music/views.py
+++ music/views.py
@@ -3,59 +3,78 @@ from django.urls import reverse
 from django.views import View
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Album, Song
 from .forms import AlbumModelForm, SongModelForm
 
-# Create your views here.
-class AlbumList(generic.ListView):
+
+class UserAlbumFilterMixin:
+    def get_queryset(self):
+        query = super().get_queryset()
+        user = self.request.user
+        return query.filter(user=user)
+
+
+class AlbumList(LoginRequiredMixin, UserAlbumFilterMixin, generic.ListView):
     template_name = "music/index.html"
     model = Album
     context_object_name = "albums"
 
 
-class AlbumDetail(generic.DetailView):
+class AlbumDetail(LoginRequiredMixin, UserAlbumFilterMixin, generic.DetailView):
     template_name = "music/detail.html"
     model = Album
 
 
-class AlbumDelete(generic.DeleteView):
+class AlbumDelete(LoginRequiredMixin, UserAlbumFilterMixin, generic.DeleteView):
     model = Album
 
     def get_success_url(self):
         return reverse("music:index")
 
 
-class AlbumCreate(generic.CreateView):
+class AlbumCreate(LoginRequiredMixin, generic.CreateView):
     form_class = AlbumModelForm
     template_name = "music/create_album.html"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
     def get_success_url(self):
         return reverse("music:index")
 
 
-class AlbumFavorite(View):
+class AlbumFavorite(LoginRequiredMixin, View):
     def get(self, request, pk):
-        album = get_object_or_404(Album, pk=pk)
+        album = get_object_or_404(Album, pk=pk, user=self.request.user)
         album.is_favorite = not album.is_favorite
         album.save()
         return JsonResponse({"success": True})
 
 
-class SongList(generic.ListView):
+# Song
+class UserSongFilterMixin:
+    def get_queryset(self):
+        query = super().get_queryset()
+        user = self.request.user
+        return query.filter(album__user=user)
+
+
+class SongList(LoginRequiredMixin, UserSongFilterMixin, generic.ListView):
     model = Song
-    context_object_name = "songs"
+    context_object_name = "song_list"
     template_name = "music/songs.html"
 
 
-class SongCreate(generic.CreateView):
+class SongCreate(LoginRequiredMixin, UserSongFilterMixin, generic.CreateView):
     form_class = SongModelForm
     template_name = "music/create_song.html"
 
     def initial_album(self):
         self.album_id = self.kwargs.get("album_id")
-        self.album = get_object_or_404(Album, pk=self.album_id)
+        self.album = get_object_or_404(Album, pk=self.album_id, user=self.request.user)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -73,22 +92,24 @@ class SongCreate(generic.CreateView):
         return reverse("music:detail", args=[self.album_id])
 
 
-class SongFavorite(View):
+class SongFavorite(LoginRequiredMixin, View):
     def get(self, request, pk):
-        song = get_object_or_404(Song, pk=pk)
+        song = get_object_or_404(Song, pk=pk, album__user=self.request.user)
         song.is_favorite = not song.is_favorite
         song.save()
         return JsonResponse({"success": True})
 
 
-class SongDelete(generic.DeleteView):
+class SongDelete(LoginRequiredMixin, UserSongFilterMixin, generic.DeleteView):
     model = Song
 
     def get_object(self):
         self.album_id = self.kwargs.get("album_id")
         self.song_id = self.kwargs.get("song_id")
 
-        return get_object_or_404(self.model, id=self.song_id, album_id=self.album_id)
+        return get_object_or_404(
+            self.get_queryset(), id=self.song_id, album_id=self.album_id
+        )
 
     def get_success_url(self):
         return reverse("music:detail", args=self.album_id)
