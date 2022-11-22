06ba35c84fdd6bb0c0f586bb687d52f824a25b62
Soroush mohammadi <sorooshm78@gmail.com>
2022-11-22 17:49:54 +0330

    remove unuse import

--- song/models.py
+++ song/models.py
@@ -3,7 +3,6 @@ import os
 from django.db import models
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
-from django.contrib.auth.models import User
 
 from album.models import Album
 
