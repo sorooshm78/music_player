1381769b705dd380f0fb943754f9f902cfb683b8
Soroush mohammadi <sorooshm78@gmail.com>
2022-11-26 18:18:31 +0330

    add home page and redirect to index page

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
@@ -24,6 +25,7 @@ urlpatterns = [
     path("album/", include("album.urls")),
     path("song/", include("song.urls")),
     path("accounts/", include("accounts.urls")),
+    path("", RedirectView.as_view(pattern_name="index")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
